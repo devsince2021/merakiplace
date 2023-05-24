c09092c6e15ad7313ac488ff986d6aa9f890f879
devsince2021 <dev.ryu2021@gmail.com>
2023-05-25 08:51:40 +0900

    최초 진입시 라우팅 로직 추가 (#5)

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+import _ from "lodash";
+
+import { Path } from "../constants";
 import Page from "../components/layouts/Page";
-import Navigation from "../components/layouts/Navigation";
-import { Outlet } from "react-router-dom";
+import BottomNavigation from "../components/navigations/BottomNavigation";
+
+const getPathname = () => {
+  return Path.home;
+};
 
 const Root = () => {
+  const [destination, setDestination] = useState<Path>();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const path = getPathname();
+    setDestination(path);
+  }, []);
+
+  useEffect(() => {
+    if (!_.isNil(destination)) {
+      navigate(destination);
+    }
+  }, [destination]);
+
   return (
     <Page>
       <Outlet />
-      <Navigation />
+      <BottomNavigation />
     </Page>
   );
 };
