551f06f53f25329ebf3a2bcd70b9ffdb769ac6b7
devsince2021 <dev.ryu2021@gmail.com>
2023-05-27 11:10:37 +0900

    이미지 추가 (#18)

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -10,4 +10,8 @@ export const Images = {
   scrap_off: attachBaseUrl("scrap_off@2x.png"),
   home_on: attachBaseUrl("home_on@2x.png"),
   home_off: attachBaseUrl("home_off@2x.png"),
+  search: attachBaseUrl("search@2x.png"),
+  search_blue: attachBaseUrl("search_blue@2x.png"),
+  calendar: attachBaseUrl("calendar_check@2x.png"),
+  calendar_blue: attachBaseUrl("calendar_check_blue@2x.png"),
 };
