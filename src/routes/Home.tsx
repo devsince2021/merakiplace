66104b8aef7713d243a7d7f48aba29d9813a7fab
devsince2021 <dev.ryu2021@gmail.com>
2023-05-27 19:23:43 +0900

    검색 api 구현 (#22)

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,4 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { getArticle } from "../apis/apiActions";
+
+const testParams = {
+  date: "2023-05-20",
+  keyword: "samsung",
+  countries: ["CANADA", "RUSSIA"],
+  page: 0,
+};
 
 const Home = () => {
   return <div>home</div>;
