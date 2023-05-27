572580711926438ddfe32d65e545cb3845184517
devsince2021 <dev.ryu2021@gmail.com>
2023-05-28 00:19:24 +0900

    이미지 생성시 컬러명 반영 (#24)

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -6,12 +6,12 @@ const baseUrl =
 const attachBaseUrl = (path: string) => `${baseUrl}/${path}`;
 
 export const Images = {
-  scrap_on: attachBaseUrl("scrap_on@2x.png"),
-  scrap_off: attachBaseUrl("scrap_off@2x.png"),
-  home_on: attachBaseUrl("home_on@2x.png"),
-  home_off: attachBaseUrl("home_off@2x.png"),
-  search: attachBaseUrl("search@2x.png"),
+  scrap_white: attachBaseUrl("scrap_white@2x.png"),
+  scrap_gray: attachBaseUrl("scrap_gray@2x.png"),
+  home_white: attachBaseUrl("home_white@2x.png"),
+  home_gray: attachBaseUrl("home_gray@2x.png"),
+  search_gray: attachBaseUrl("search_gray@2x.png"),
   search_blue: attachBaseUrl("search_blue@2x.png"),
-  calendar: attachBaseUrl("calendar_check@2x.png"),
+  calendar_gray: attachBaseUrl("calendar_check_gray@2x.png"),
   calendar_blue: attachBaseUrl("calendar_check_blue@2x.png"),
 };
