da9e8bfe928d8ecaeee55f1fd5974f94b5e8a4e3
devsince2021 <dev.ryu2021@gmail.com>
2023-05-28 09:51:17 +0900

    styled-components 적용 위치 변경 (#26)

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import appRouter from "./routes";
+import { Provider } from "react-redux";
+import { ThemeProvider } from "styled-components";
 
+import store from "./redux";
+import appRouter from "./routes";
+import { theme } from "./constants/styles";
 import "./index.css";
 
 const container = document.getElementById("root");
@@ -10,6 +14,10 @@ const root = createRoot(container!);
 
 root.render(
   <React.StrictMode>
-    <RouterProvider router={appRouter} />
+    <ThemeProvider theme={theme}>
+      <Provider store={store}>
+        <RouterProvider router={appRouter} />
+      </Provider>
+    </ThemeProvider>
   </React.StrictMode>
 );
